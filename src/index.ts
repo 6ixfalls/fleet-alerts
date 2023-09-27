f8cdfc6ec39a617628a25743bf158eff9e65171e
Six <23470032+6ixfalls@users.noreply.github.com>
2023-09-26 22:38:47 -0700

    test running version

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,42 @@ const apiVersion = process.env.API_VERSION || "fleet.cattle.io/v1alpha1";
 const group = apiVersion.split("/")[0];
 const version = apiVersion.split("/")[1];
 
+const StateMap: {
+    [state: string]: {
+        state: "error" | "success" | "pending" | "failure";
+        description: string;
+    };
+} = {
+    Ready: {
+        state: "success",
+        description: "Bundle deployment succeeded",
+    },
+    NotReady: {
+        state: "pending",
+        description: "Bundle deployment in progress",
+    },
+    WaitApplied: {
+        state: "pending",
+        description: "Bundle deployment in progress",
+    },
+    ErrApplied: {
+        state: "error",
+        description: "Bundle deployment failed",
+    },
+    OutOfSync: {
+        state: "error",
+        description: "Bundle deployment OutOfSync",
+    },
+    Pending: {
+        state: "pending",
+        description: "Bundle deployment in progress",
+    },
+    Modified: {
+        state: "success", // failure?
+        description: "Bundle deployment modified",
+    },
+};
+
 const octokit = new Octokit({ auth: process.env.GITHUB_ALERTS_PAT });
 const {
     data: { login },
@@ -24,19 +60,73 @@ const informer = k8s.makeInformer(
         k8sApi.listClusterCustomObject(group, version, "bundledeployments")
 );
 
-informer.on("add", async (obj) => {
-    logger.info(`BundleDeployment ${obj.metadata?.name} added`);
-    logger.info(obj);
-});
+const watch = new k8s.Watch(kc);
+const gitRepoRefs: { [name: string]: { user: string; repo: string } } = {};
+const request = await watch.watch(
+    `/apis/${apiVersion}/gitrepos`,
+    {},
+    (type, obj) => {
+        if (type === "ADDED") {
+            const match = obj.spec.repo.match(
+                /git@github\.com:(.*)\/(.*)\.git/
+            ); // git@github.com:username/repo.git
+            gitRepoRefs[obj.metadata.name] = {
+                user: match[1],
+                repo: match[2],
+            };
+        } else if (type === "MODIFIED") {
+            const match = obj.spec.repo.match(
+                /git@github\.com:(.*)\/(.*)\.git/
+            );
+            gitRepoRefs[obj.metadata.name] = {
+                user: match[1],
+                repo: match[2],
+            };
+        } else if (type === "DELETED") {
+            delete gitRepoRefs[obj.metadata.name];
+        }
+    },
+    (err) => logger.error(err)
+);
 
-informer.on("update", async (obj) => {
-    logger.info(`BundleDeployment ${obj.metadata?.name} updated`);
-    logger.info(obj);
-});
+async function updateBundleDeployment(
+    obj: k8s.KubernetesObject & { [key: string]: any }
+) {
+    logger.info(`BundleDeployment ${obj.metadata?.name} state changed`);
+    if (!obj.metadata || !obj.metadata.labels) {
+        logger.error(`BundleDeployment missing metadata`);
+        return;
+    }
+    const gitRepoRef =
+        gitRepoRefs[obj.metadata.labels["fleet.cattle.io/repo-name"]];
+    if (!gitRepoRef) {
+        logger.error(
+            `GitRepoRef ${obj.metadata.labels["fleet.cattle.io/repo-name"]} not found`
+        );
+        return;
+    }
+
+    const state = StateMap[obj.status?.state];
+
+    octokit.rest.repos.createCommitStatus({
+        owner: gitRepoRef.user,
+        repo: gitRepoRef.repo,
+        sha: obj.metadata.labels["fleet.cattle.io/commit"],
+        context: obj.metadata.name,
+        state: state.state,
+        description: state.description,
+        target_url:
+            process.env.RANCHER_EXPLORER_URL &&
+            `${process.env.RANCHER_EXPLORER_URL}/${group}.bundledeployment/${obj.metadata.namespace}/${obj.metadata.name}`,
+    });
+}
+
+informer.on("add", updateBundleDeployment);
+
+informer.on("update", updateBundleDeployment);
 
 informer.on("delete", async (obj) => {
     logger.info(`BundleDeployment ${obj.metadata?.name} deleted`);
-    logger.info(obj);
 });
 
 informer.on("error", (err) => {
